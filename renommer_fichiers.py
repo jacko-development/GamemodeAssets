d61ad950d599d29d17d458a36942dc1a3a8afb6c
jacko-development <157727900+jacko-development@users.noreply.github.com>
2024-10-17 18:24:17 +0200

    update

--- renommer_fichiers.py
+++ renommer_fichiers.py
@@ -1,13 +1,13 @@
 import os
 
 # Chemin vers le dossier contenant les images
-dossier = 'C:\\Users\\natha\\Desktop\\GamemodeAssets\\clothing\\male\\torso'
+dossier = 'C:\\Users\\natha\\Desktop\\GamemodeAssets\\clothing\\male\\tshirts'
 # Parcours de tous les fichiers dans le dossier
 for filename in os.listdir(dossier):
     # Vérifie si le fichier commence par "male_5_"
-    if filename.startswith('male_11_'):
+    if filename.startswith('male_8_'):
         # Extraire la partie du fichier après "male_5_"
-        new_name = filename.replace('male_11_', '', 1)
+        new_name = filename.replace('male_8_', '', 1)
         # Renommer le fichier
         os.rename(os.path.join(dossier, filename), os.path.join(dossier, new_name))
 
